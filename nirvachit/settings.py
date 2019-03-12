2db24bafbdceb341e77626e685026839760b5a17
gautamamber <ambergautam1@gmail.com>
2019-03-12 12:07:01 +0530

    Send news letter from admin'

--- nirvachit/settings.py
+++ nirvachit/settings.py
@@ -31,7 +31,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'gautamamber5@gmail.com'
-EMAIL_HOST_PASSWORD = '***************'
+EMAIL_HOST_PASSWORD = '**************'
 EMAIL_PORT = 587
 # Application definition
 
--- pollapp/admin.py
+++ pollapp/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from .models import Post, Comment, News, Poll, Subscriber, SendNewsLetter
+from django.core.mail import send_mail
+
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(News)
 admin.site.register(Subscriber)
-admin.site.register(SendNewsLetter)
-@admin.register(Poll)
 
+@admin.register(Poll)
 class PollAdmin(admin.ModelAdmin):
 	readonly_fields = ['first_poll_count', 'second_poll_count']
+
+@admin.register(SendNewsLetter)
+class SendNewsLetterAdmin(admin.ModelAdmin):
+	def save_model(self, request, obj, form, change):
+		all_subscriber = Subscriber.objects.all()	
+		email = []
+		for i in all_subscriber:
+			email.append(i.email)
+		complete_mail = obj.title + "\n" + obj.text + "\n" + obj.links
+		send_mail(obj.title, complete_mail, 'gautamamber5@gmail.com', email)
+		super().save_model(request, obj, form, change)
